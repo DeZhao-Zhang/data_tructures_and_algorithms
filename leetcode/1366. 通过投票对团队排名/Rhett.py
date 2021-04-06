95363f631e83bdf89052ddd907865bae46b8abb2
Jhsy <jhsyzxy@gmail.com>
2021-04-06 15:09:19 +0800

    尝试添加1366的rhett解答

---
+++ leetcode/1366. 通过投票对团队排名/Rhett.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+# Author: Jhsy
+# @Time: 2021/4/2 21:10
+
+class Solution(object):
+    def rankTeams(self, votes):
+        """
+        :type votes: List[str]
+        :rtype: str
+        """
