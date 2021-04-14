89797406aa2d184b61d7d23af88d27491304fbaa
DeZhao-Zhang <jhsyzxy@gmail.com>
2021-04-14 19:07:43 +0800

    Update Rhett.py

--- leetcode/226. 翻转二叉树/Rhett.py
+++ leetcode/226. 翻转二叉树/Rhett.py
@@ -1,3 +1,17 @@
 # -*- coding:utf-8 -*-
 # Author: Jhsy
 # @Time: 2021/4/14 18:56
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return
+
+        temp = root.left
+        root.left = self.invertTree(root.right)
+        root.right = self.invertTree(temp)
+        return root
