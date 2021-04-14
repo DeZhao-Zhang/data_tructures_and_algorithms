c1a0f6519cc503744e11a0446b9ba73326d9c5a8
DeZhao-Zhang <jhsyzxy@gmail.com>
2021-04-14 20:45:29 +0800

    Update Rhett.py

--- leetcode/114. 二叉树展开为链表/Rhett.py
+++ leetcode/114. 二叉树展开为链表/Rhett.py
@@ -1,3 +1,29 @@
 # -*- coding:utf-8 -*-
 # Author: Jhsy
 # @Time: 2021/4/14 19:16
+
+class Solution(object):
+    def flatten(self, root):
+        """
+        :type root: TreeNode
+        :rtype: None Do not return anything, modify root in-place instead.
+        """
+        if not root:
+            return 
+        
+        self.flatten(root.left)
+        self.flatten(root.right)
+
+        left = root.left
+        right = root.right
+
+        root.left = None
+        root.right = left
+
+        p = root
+        while p.right != None:
+            p = p.right
+        
+        p.right = right
+
+        return root
