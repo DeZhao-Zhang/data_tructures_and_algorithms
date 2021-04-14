89b4e9964088a8039dccbe3d11f4c579291fe6d4
DeZhao-Zhang <jhsyzxy@gmail.com>
2021-04-14 21:10:40 +0800

    Update Rhett.py

--- leetcode/116. 填充每个节点的下一个右侧节点指针/Rhett.py
+++ leetcode/116. 填充每个节点的下一个右侧节点指针/Rhett.py
@@ -1,3 +1,28 @@
 # -*- coding:utf-8 -*-
 # Author: Jhsy
 # @Time: 2021/4/14 20:48
+
+class Solution(object):
+    def connect(self, root):
+        """
+        :type root: Node
+        :rtype: Node
+        """
+
+        if not root:
+            return
+        else:
+            self.connect_two_node(root.left, root.right)
+
+        return root
+
+
+    def connect_two_node(self, node1, node2):
+        if not node1:
+            return
+
+        node1.next = node2
+
+        self.connect_two_node(node1.left, node1.right)
+        self.connect_two_node(node1.right, node2.left)
+        self.connect_two_node(node2.left, node2.right)
