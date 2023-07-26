d380db36cae4f343b38c460a8f46af0188a3665c
DeZhao-Zhang <jhsyzxy@gmail.com>
2023-07-26 09:35:25 +0800

    (feature) 1.新增递归的方式实现链表反转

--- fucking-algorithm/反转链表.js
+++ fucking-algorithm/反转链表.js
@@ -4,10 +4,11 @@ import Linked from "../fucking-algorithm/Linked.js";
 let myLinked = new Linked();
 
 myLinked.add(5);
-
 myLinked.add(6);
-
 myLinked.add(14);
+myLinked.add(17);
+myLinked.add(21);
+
 
 myLinked.print()
 
@@ -29,4 +30,25 @@ while (cur != null){
     cur = next;
 }
 
+myLinked.print()
+
+// 递归的方式 进行链表的反转
+// 什么叫做递归？？设定一个截至条件，在达到截止条件之前，先解决子问题
+
+function inverse(node){
+    if (node.next == null){
+        return node;
+    }
+
+    // 通过last将链表最后的节点返回回去 并作为最后的头节点
+    let last = inverse(node.next);
+
+    // 将指向顺序反转
+    node.next.next = node;
+    // 打算循环嵌套
+    node.next = null;
+    return last;
+}
+
+myLinked.head = inverse(myLinked.head)
 myLinked.print()
